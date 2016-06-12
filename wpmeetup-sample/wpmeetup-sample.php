1b2b7482b6f00ef59e68030519d1ab28d2c1fc13
Mayuko Moriyama <mayuko.jpn@gmail.com>
2016-06-12 11:42:14 +0900

    initial commit.

---
+++ wpmeetup-sample/wpmeetup-sample.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: WP Meetup Tokyo
+Description: Short description of your plugin.
+Author: Your Name
+Version: 1.0
+Author URI: http://www.meetup.com/WordPress-Meetup-Tokyo
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Create a function.
+function wpmeetup_function () {
+	// Write some code here!
+
+}
+
+// Hook a function.
+add_action( 'wp_footer', 'wpmeetup_function' );
